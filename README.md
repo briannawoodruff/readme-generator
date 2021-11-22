30c292d713518b8b7ea16d8768e77c9b618c533d
Brianna Woodruff <briannaewoodruff@gmail.com>
2021-11-22 02:02:54 -0600

    functioning readme generator

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# readme-generator
+
+  # README Generator
+
+  ## Description:
+  ***
+  This is a README Generator made with Node JS and Inquirer.
+
+  ## Table of Contents:
+  ***
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributors](#contributors)
+
+  ## Installation:
+  ***
+  N/A
+
+  ## Usage:
+  ***
+  To create professional README's more efficiently.
+
+  ## Licenses:
+  ***
+  ###  MIT License
+
+  ## Contributors:
+  ***
+  briannawoodruff
+
+  ## Username:
+  ***
+  briannawoodruff
+  
+  ## Project Repo:
+  ***
+  readme-generator
+   
+  
--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   
+        type: "input",
+        message: "What is your GitHub username",
+        name: "username"
+    },
+    {   
+        type: "input",
+        message: "What is your GitHub repo?",
+        name: "repo"
+    },
+    {   
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {  
+        type: "input",
+        message: "Write a description for your project.",
+        name: "description"
+    },
+    {   
+        type: "input",
+        message: "What is the installation process?",
+        name: "installation"
+    },
+    {   
+        type: "input",
+        message: "How will this project be used?",
+        name: "usage"
+    },
+    {   
+        type: "checkbox",
+        message: "What licenses are required with this project?",
+        name: "licenses",
+        choices: [' MIT', ' GPLv3', ' Apache 2.0', " None"]
+    },
+    {   
+        type: "input",
+        message: "Who were the contributors of this project?",
+        name: "contributors"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const markdown = generateMarkdown(data);
+    console.log(markdown)
+    console.log(data.licenses)
+
+    fs.writeFile(fileName, markdown, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile("README.md", data)
+    })
+}
 
 // Function call to initialize app
 init();
+ 
